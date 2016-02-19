1043bfc0be70a9c0bafd75356f53ea69c4bb3411
wihika <wihika@hotmail.com>
2016-02-19 05:10:29 +0000

    adapting to the tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
     has_many :task
     validates :fl_name, length: {maximum: 30,  message: "Invalid name" }, presence: true
     validates :password, length: {maximum: 30,  message: "Invalid password" }, presence: true, confirmation: true
-    validates :password_confirmation, presence:true
+    validates :password_confirmation, presence:true, length: {maximum: 30,  message: "Invalid password" }, presence: true, confirmation: true
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, length: {maximum: 30}, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true , uniqueness: { case_sensitive: false, message: "Account with this email already exists!"}
 end
